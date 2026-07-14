9e5115b12df5cb99150c734d4f7d4afeafe3c610
Sanju <sanjumalik2006@gmail.com>
2026-07-14 17:25:07 +0530

    Time: 1 ms (99.98%) | Memory: 45.5 MB (56.86%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -14,31 +14,33 @@
  * }
  */
 class Solution {
-    public void  helper(TreeNode root,List<List<Integer>> ans, List<Integer> currPath, int targetSum) {
-         if(root==null) return;
-           if(root!=null && root.left==null && root.right==null) {
-            currPath.add(root.val);
-            if(root.val==targetSum)
-            {
-              List<Integer> a=new ArrayList<>();
-              for(int i=0;i<currPath.size();i++)
-              {
-                a.add(currPath.get(i));
-              }
-              ans.add(a);
-            }
-            currPath.remove(currPath.size()-1);
-            return;
+    public void helper(TreeNode root,  List<List<Integer>> ans,List<Integer>currPath ,int targetSum){
+    if(root==null) return;
+    if(root!=null && root.left==null && root.right==null)
+    {
+         currPath.add(root.val);
+        if(targetSum==root.val)
+        {
+           List<Integer> a=new ArrayList<>();
+           for(int i=0;i<currPath.size();i++)
+           {
+            a.add(currPath.get(i));
            }
-           currPath.add(root.val);
-           helper(root.left,ans,currPath,targetSum-root.val);
-            helper(root.right,ans,currPath,targetSum-root.val);
-      currPath.remove(currPath.size()-1);
+           ans.add(a);
+
+        }
+        currPath.remove(currPath.size()-1);
+    }
+     currPath.add(root.val);
+     helper(root.left,ans,currPath,targetSum-root.val);
+     helper(root.right,ans,currPath,targetSum-root.val);
+     currPath.remove(currPath.size()-1);
     }
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> ans =new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         List<Integer> currPath=new ArrayList<>();
-         helper(root,ans,currPath,targetSum);
-         return ans;
+        helper(root,ans,currPath,targetSum);
+        return ans;
+
     }
 }
